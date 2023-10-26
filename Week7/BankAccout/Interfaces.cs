7521d2fce6d5886b1fe94ea9e54f6063e795498b
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 18:15:56 +0530

    7

---
+++ Week7/BankAccout/Interfaces.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /*internal interface ISavingsAccount
+    {
+        double CalculateSavingsInterest();
+    }
+    
+    internal interface ICurrentAccount
+    {
+        double CalculateCurrentInterest();
+    }
+
+    
+    internal interface IGoldLoanAccount
+    {
+        double CalculateGoldLoanInterest();
+    }*/
+
+
+    //Implementing the interaces
+    class BankAccount : ISavingsAccount, ICurrentAccount, IGoldLoanAccount
+    {
+        public double Balance { get; set; }
+        public double CalculateSavingsInterest()
+        {
+            return Balance * 0.09;
+        }
+        public double CalculateCurrentInterest()
+        {
+            return Balance * 0.07;
+        }
+        public double CalculateGoldLoanInterest()
+        {
+            return Balance * 0.04;
+        }
+    }
+}
---
+++ Week7/BankAccout/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("enter the balance");
+            account.Balance = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("choose below options to calculate interest");
+                Console.WriteLine("1.Savings Account");
+                Console.WriteLine("2.Current Account");
+                Console.WriteLine("3.Gold Loan Account");
+                Console.WriteLine("4.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+
+                        Console.WriteLine($"Interest for savings account:{account.CalculateSavingsInterest()}");
+
+                        break;
+
+                    case 2:
+
+                        Console.WriteLine($"Interest for current account:{account.CalculateCurrentInterest()}");
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"Interest for Gold loan account:{account.CalculateGoldLoanInterest()}");
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine("exiting");
+
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice");
+
+                        break;
+
+                }
+
+
+
+            }
+        }
+
+    }
+
+}
+
+    
