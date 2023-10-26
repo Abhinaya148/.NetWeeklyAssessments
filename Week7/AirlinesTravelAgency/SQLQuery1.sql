76c31915e216e94f7b8b6bfe17473ed7c8b4d7b9
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 18:14:26 +0530

    7

---
+++ Week7/AirlinesTravelAgency/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿create table Passenger
+(
+PassengerId int primary key not null,
+ReservationId int foreign key references Reservations(ReservationId),
+PassengerName varchar(100) 
+);
+
+ 
+
+drop table Passenger;
